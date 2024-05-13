54bbe61e4e96d3c6228c921fd8f0ed3262f9ab7e
WhiteShadow31 <quentin31.delon@gmail.com>
2024-05-13 15:58:50 +0200

    commit slow motion sans le controller system

--- Assets/Scripts_QD/Sc_HitEnnemi.cs
+++ Assets/Scripts_QD/Sc_HitEnnemi.cs
@@ -1,22 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Sc_HitEnnemi : MonoBehaviour
 {
 
-    public GameObject mob;
+    //public GameObject mob;
     public float impultionForce = 15f;
     //public Vector3 impulseVector = new Vector3(10f, 10f, 0f);
 
-    void Start()
-    {
+    public int nbHit = 0;
 
+    //Slow Motion variables
+    public float slowMotionTimeScale;
+
+    private float startTimeScale;
+    private float startFixedDeltaTime;
+
+    void Start() {
+
+        startTimeScale = Time.timeScale;
+        startFixedDeltaTime = Time.fixedDeltaTime;
     }
 
-    void Update()
-    {
+    void Update() {
 
+        //var gamepad = Gamepad.current;
+
+        if (nbHit >= 5 /*&&*/  /*Input.GetButtonDown("Nom de la touche")*/) {
+
+            impultionForce = 50f;
+            StartSlowMotion();
+        } else {
+
+            StopSlowMotion();
+        }
         //Debug.Log(mob.mobDir.y);
     }
 
@@ -27,7 +46,20 @@ public class Sc_HitEnnemi : MonoBehaviour
 
             collider.rigidbody.AddForce(Vector3.forward * impultionForce, ForceMode.Impulse);
             collider.rigidbody.AddForce(Vector3.up * impultionForce, ForceMode.Impulse);
+            nbHit++;
         } 
         //GetComponent<Rigidbody>().AddForce(impulseVector, ForceMode.Impulse);
+    }
+
+    private void StartSlowMotion() {
+
+        Time.timeScale = slowMotionTimeScale;
+        Time.fixedDeltaTime = startFixedDeltaTime * slowMotionTimeScale;
+    }
+
+    private void StopSlowMotion() {
+
+        Time.timeScale = startTimeScale;
+        Time.fixedDeltaTime = startFixedDeltaTime;
     }
 }
